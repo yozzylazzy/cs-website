ac11de21f47f23a5cb538e081e10801bcbf0e204
Yozzy Lazzy <77936231+yozzylazzy@users.noreply.github.com>
2022-07-29 19:11:12 +0700

    Update Secondary BG
    Update Secondary BG

--- src/components/SecondSection.js
+++ src/components/SecondSection.js
@@ -6,7 +6,7 @@ import './SecondSection.css';
 function SecondSection() {
     return (
         <div className='hero-container'>
-            <video src='/videos/video-1.mp4' autoPlay loop muted />
+            <video src='/videos/video-2.mp4' autoPlay loop muted />
             <h1>TENTANG KAMI</h1>
             <p>BUILD WITH REACT JS <img src='logo512.png' id="reactlogo" width="45" /></p>
             <div className='hero-btns'>
